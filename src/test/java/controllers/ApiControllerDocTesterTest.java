e626c83f9a9f29bf1b0603b49eb93c7ae0a2f9b2
CHAMP <champ@live.cn>
2014-05-05 14:44:25 +0700

    add title and menu

--- src/test/java/controllers/ApiControllerDocTesterTest.java
+++ src/test/java/controllers/ApiControllerDocTesterTest.java
@@ -38,8 +38,8 @@ public class ApiControllerDocTesterTest extends NinjaDocTester {
                 Request.GET().url(
                         testServerUrl().path(URL_INDEX)));
 
-        assertThat(response.payload, containsString("HXXX"));
-        assertThat(response.payload, containsString("BAM!"));
+        assertThat(response.payload, containsString("Welcome"));
+        assertThat(response.payload, containsString("Home"));
 
 
     }
